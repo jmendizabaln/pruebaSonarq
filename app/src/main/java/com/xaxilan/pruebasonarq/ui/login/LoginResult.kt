950da4ea8e5281c101beb3a52d1cad43f6eee264
jmendizabaln <112492392+jmendizabaln@users.noreply.github.com>
2022-11-11 10:02:04 -0600

    second commit

---
+++ app/src/main/java/com/xaxilan/pruebasonarq/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.xaxilan.pruebasonarq.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
